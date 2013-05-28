a5c5c3257cd0094f203326c0eb643a60b957c520
Emily Rose <emily@contactvibe.com>
2013-05-27 17:02:10 -0700

    Combined findId and findService into lookup function parameter; now returns object. Added auth function parameter.

--- router.js
+++ router.js
@@ -7,73 +7,96 @@ var responseStream = require('response-stream')
 var through = require('through')
 var log = require('debug')('router')
 
-module.exports = function(program, socket, findId, findService) {
+module.exports = function(program, socket, lookup, auth) {
 
-  findId = findId || subdomainToId
-  findService = findService || subdomainToService
+  lookup = lookup || defaultLookup
+  auth = auth || defaultAuth
 
   var clients = program.clients
   log('new connection')
-  var parser = httppp(function(headers) {
-    var id = findId(headers)
-    var service = findService(headers)
-    var host = (headers[2].host && headers[2].host.length) ? headers[2].host[0] : null
-    var mx = clients[id]
-    if (!mx) {
-      log('client not connected', id)
-      return socket.end()
-    }
-    log('requesting service', service || '*')
-    if (!service) {
-      parser.pipe(mx.createStream(
-        'services'
-      )).pipe(through(function(data) {
-        var rs = responseStream(socket)
-        rs.writeHead(200, {'Content-Type':'text/html'})
-        rs.write(servicesList(data, host))
-        rs.end()
-      }))
-      return
-    }
-    parser.pipe(mx.createStream({
-      service: service
-    })).pipe(socket);
+  var request = httppp(function (headers) {
+
+    lookup(headers, function (err, data) {
+
+      if(err) {
+        log(err)
+        return sock.end()
+      }
+      var id = data.id
+      var host = data.host
+      var service = data.service
+
+      //TODO: auth step
+      auth(headers, function(err) {
+
+        if(err) {
+          log(err)
+          return sock.end()
+        }
+        var mx = clients[id]
+        if (!mx) {
+
+          log('client not connected', id)
+          return socket.end()
+        }
+
+        log('requesting service', service || '*')
+        if (!service) { // requesting service listing
+
+          request.pipe(mx.createStream(
+            'services'
+          )).pipe(through(function(data) {
+            var rs = responseStream(socket)
+            rs.writeHead(200, {'Content-Type':'text/html'})
+            rs.write(servicesList(data, host))
+            rs.end()
+          }))
+          return
+        }
+
+        request.pipe(mx.createStream({
+          service: service
+        })).pipe(socket);
+      })
+    })
   })
-  return parser
+  return request
 }
 
-function subdomainToId(headers) {
-  var host = (headers[2].host && headers[2].host.length) ? headers[2].host[0] : null
-  if (!host) {
-    log('could not parse host', headers[2]);
-    socket.end()
-    return
-  }
-  var hostname = host.split(":").shift() // remove port from host header
-  var subdomain = hostname.split('.')
-  var id = subdomain[1]
-  if (subdomain.length === 3) {
-    id =  subdomain[0]
-    service = false
-  }
-  return id
-}
+function defaultLookup(headers, cb) {
 
-function subdomainToService(headers) {
-  var host = (headers[2].host && headers[2].host.length) ? headers[2].host[0] : null
-  if (!host) {
-    log('could not parse host', headers[2]);
-    socket.end()
-    return
+  var host = (headers[2].host && headers[2].host.length)
+    ? headers[2].host[0]
+    : null
+  ;
+  if(!host) { // invalid headers
+
+    var err = 'could not parse host'
+    log(err, headers[2])
+    return cb(new Error(err))
   }
-  var hostname = host.split(":").shift() // remove port from host header
+
+  var hostname = host.split(':').shift()  // remove port from host header
   var subdomain = hostname.split('.')
   var service = subdomain[0]
-  if (subdomain.length === 3) {
+  var id = subdomain[1]
+
+  if(subdomain.length === 3) {
+
+    id = subdomain[0]
     service = false
   }
-  return service
+  cb(null, {
+
+    host : host
+    , hostname : hostname // no port
+    , subdomain : subdomain
+    , service : service
+    , id : id
+  })
 }
+
+function defaultAuth(headers, cb) { cb(null) }
 
 function servicesList(data, host) {
   var a = [
